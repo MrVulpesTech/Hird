f7caa92e1f361563e8d23dce62a70f291a325123
Mr Vulpes <mr.vulpes.tech@gmail.com>
2024-10-25 10:33:04 +0300

    Updated the structure and texts

--- script.js
+++ script.js
@@ -19,34 +19,6 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
     });
 
-    // Додавання анімації при скролі
-    $(window).on('scroll', function() {
-        $('.unit-card').each(function() {
-            var cardTop = $(this).offset().top;
-            var scrollTop = $(window).scrollTop();
-            var windowHeight = $(window).height();
-            if (scrollTop + windowHeight - 100 > cardTop) {
-                $(this).addClass('animate-fadeInUp');
-            }
-        });
-    });
+
 });
 
-// Функція для перевірки, коли елемент з'являється на екрані
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Додаємо клас "visible", коли користувач долистує до другого блоку
-window.addEventListener('scroll', function() {
-    const secondBlock = document.getElementById('second-block');
-    if (isInViewport(secondBlock)) {
-        secondBlock.classList.add('visible');
-    }
-});
